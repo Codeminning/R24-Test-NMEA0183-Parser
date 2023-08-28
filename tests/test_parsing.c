40771b472c1a967f904f5fab3b95771e973b567d
Dhyanesh <141729234+Codeminning@users.noreply.github.com>
2023-08-28 05:48:42 -0700

    Update test_parsing.c

--- tests/test_parsing.c
+++ tests/test_parsing.c
@@ -3,12 +3,14 @@
 #include "unity.h"
 #include "unity_internals.h"
 
-void test_gga_parsing_over_examples(void) {
-  const char* examples[] = {
+void test_gga_parsing_over_examples(void) 
+{
+  const char* examples[] = 
+  {
     "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,0000*5E"
   }
-  gps_t handle = gps_init();
-  TEST_ASSERT_EQUAL(GPS_NO_ERROR, gps_update(handle, examples[0], strlen(examples[0])));
+  gps_init handle = gps_init()
+  TEST_ASSERT_EQUAL(GPS_NO_ERROR, gps_update(handle, examples[0], strlen(examples[0])))
   int latlon_degmins[2];
   int latlon_minfracs[2];
   gps_get_lat_lon(handle, latlon_degmins, latlon_minfracs);
